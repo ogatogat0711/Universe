1496a3408f74872f83630036b33e6a7c120360e4
ogatogat0711 <shunsuketago@icloud.com>
2025-07-23 19:17:21 +0900

    Change Probe model, some test

--- Assets/Scripts/Probe.cs
+++ Assets/Scripts/Probe.cs
@@ -9,11 +9,13 @@ public class Probe : MonoBehaviour
     private Vector3 _velocity;
     private float _horizontal;
     private float _vertical;
+    private float _rotationSpeed = 25f;
     //public Camera followingCamera;
     public CinemachineVirtualCameraBase followingVirtualCamera;
     public GameObject collisionTarget;
-    public bool canMove;//移動可能かどうかのフラグ. UIボタンで制御
+    public bool canMove;//移動可能かどうかのフラグ
     public bool isManipulating; // 操作中かどうかのフラグ
+    private bool _notNeedToRotate;//回転が必要かのフラグ
     public int fuel; // 燃料
     public int maxFuel = 100;// 最大燃料
     public int fuelConsumptionRatioOfManipulation = 3; // 操作時の燃料消費率
@@ -25,6 +27,7 @@ public class Probe : MonoBehaviour
     {
         fuel = maxFuel;
         _rigidbody = GetComponent<Rigidbody>();
+        _notNeedToRotate = true;
         isManipulating = false;
         isClear = false;
     }
@@ -32,10 +35,29 @@ public class Probe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 cameraDirection = followingVirtualCamera.transform.forward;
+        
         if (canMove)
         {
             _horizontal = Input.GetAxis("Horizontal");
             _vertical = Input.GetAxis("Vertical");
+            
+            Vector3 headingDirection = cameraDirection * _vertical + followingVirtualCamera.transform.right * _horizontal;//向かう方向
+            headingDirection.Normalize();
+
+            if (_vertical != 0)
+            {
+                _notNeedToRotate = CheckRotation(headingDirection);//回転判定
+            }
+
+            if (!_notNeedToRotate)
+            {
+                Quaternion targetRotation = new Quaternion();
+                targetRotation.SetFromToRotation(transform.forward, headingDirection);//向かう方向の回転
+
+                transform.rotation =
+                    Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+            }
             
             if(_horizontal != 0 || _vertical != 0)
             {
@@ -55,11 +77,12 @@ public class Probe : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (canMove)
+        if (canMove && _notNeedToRotate)
         {
             Vector3 cameraDirection= followingVirtualCamera.transform.forward;//カメラの見ている方向からXZ平面の単位ベクトルを取得
             Vector3 moveDirection = cameraDirection * _vertical + followingVirtualCamera.transform.right * _horizontal;//キー入力から移動方向を決定
             moveDirection *= Time.fixedDeltaTime;
+            
             _rigidbody.linearVelocity = moveDirection * speed;
             _fuelConsumption = Mathf.RoundToInt(Mathf.Abs(_horizontal) + Mathf.Abs(_vertical));
             _fuelConsumption *= fuelConsumptionRatioOfManipulation; // 燃料消費量を計算
@@ -76,5 +99,13 @@ public class Probe : MonoBehaviour
             isClear = true;
             //Time.timeScale = 0f;
         }
+    }
+
+    private bool CheckRotation(Vector3 direction)
+    {
+        float theta = Vector3.Angle(transform.forward, direction);
+        Debug.Log("theta: " + theta);
+        
+        return theta <= 5f;//5度以内なら回転しない
     }
 }
---
+++ Assets/Scripts/TestMove.cs
@@ -0,0 +1,71 @@
+using TMPro;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+    public GameObject testObject;
+    public TMP_InputField xField;
+    public TMP_InputField yField;
+    public TMP_InputField zField;
+    public TMP_Text errorText;
+    private Vector3 _inputVector;
+    private bool _needToRotate;
+
+    void Start()
+    {
+        _inputVector = testObject.transform.position;
+        _needToRotate = false;
+    }
+    void Update()
+    {
+        Debug.DrawRay(testObject.transform.position, testObject.transform.forward * 10, Color.blue);
+        Debug.DrawRay(testObject.transform.position, _inputVector.normalized * 10, Color.red);
+
+        if (_needToRotate)
+        {
+            Quaternion nextRotation = Quaternion.RotateTowards(testObject.transform.rotation,
+                Quaternion.LookRotation(_inputVector), 50f * Time.deltaTime);
+            
+            testObject.transform.rotation = nextRotation;
+        }
+
+        _needToRotate = CheckRotation(_inputVector);
+    }
+
+    public void OnClick()
+    {
+        string inputX = xField.text;
+        string inputY = yField.text;
+        string inputZ = zField.text;
+        
+        if(float.TryParse(inputX, out float x) && float.TryParse(inputY, out float y) && float.TryParse(inputZ, out float z))
+        {
+            _inputVector = new Vector3(x, y, z);
+            Debug.Log(_inputVector);
+            
+            errorText.text = ""; // エラーメッセージをクリア
+            
+            if (x == 0 && y == 0 && z == 0)
+            {
+                errorText.text = "Zero vector is prohibited.";
+            }
+            
+            
+            
+        }
+        else
+        {
+            errorText.text = "Invalid input.\n Please enter valid numbers.";
+        }
+    }
+    
+    private bool CheckRotation(Vector3 headingDirection)
+    {
+        // 回転が必要かどうかを判定する
+        
+        float angle = Vector3.Angle(testObject.transform.forward, headingDirection);
+   
+        Debug.Log("angle: " + angle);
+        return angle > 0.5f; // 2度より大きいなら回転が必要とする
+    }
+}
